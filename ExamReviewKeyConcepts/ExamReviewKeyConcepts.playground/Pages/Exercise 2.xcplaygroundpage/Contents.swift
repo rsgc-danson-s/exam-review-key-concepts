8a591063f1baeb2d20986df9307891a129a69675
Student <student@macbook-pro-0863-sebastian-danson.local>
2017-05-17 10:33:45 -0400

    finished second exercise

--- ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise 2.xcplaygroundpage/Contents.swift
+++ ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise 2.xcplaygroundpage/Contents.swift
@@ -23,6 +23,18 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
 
+canvas.translate(byX: 0, byY: 350)
+
+
+for x in stride(from: 100, to: 0, by: -5) {
+    canvas.textColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: x)
+    canvas.drawText(message: "undertones", size: 70, x: 7, y: -40)
+    canvas.rotate(by: -5)
+   
+    
+    
+}
+
 
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
